dd2f7ffcc99664ee43712a27cee686e39dab888f
camillexue <xue.camille@gmail.com>
2017-04-20 14:45:56 -0400

    Audio now doesn't save wave file, just gets amplitude directly from stream

--- voiceAmplitude.py
+++ voiceAmplitude.py
@@ -8,80 +8,70 @@ import pylab as pl
 import time
 import threading
 import queue
+import math
+import struct
+
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 RECORD_SECONDS = .5
+SHORT_NORMALIZE = (1.0/32768.0)
 WAVE_OUTPUT_FILENAME = "output.wav"
-THRESHOLD = 0
+THRESHOLD = .02
 p = pyaudio.PyAudio()
 recordingQueue = queue.Queue()
 
 trigger = False
 
 
-def record(record_seconds):
-    """
-    Records audio and returns frames to be saved to wav file
-    """
-    frames = []
+def get_rms(block):
+    count = len(block)/2
+    format = "%dh"%(count)
+    shorts = struct.unpack(format, block)
+
+    sum_squares = 0.0
+    for sample in shorts:
+        n = sample * SHORT_NORMALIZE
+        sum_squares += n*n
+
+    return math.sqrt(sum_squares / count)
+
+
+def open_stream():
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
                     input=True,
                     frames_per_buffer=CHUNK)
-
-    print("*RECORDING")
-
-    for i in range(0, int(RATE / CHUNK * record_seconds)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    print("*DONE RECORDING")
+    block = stream.read(CHUNK)
 
     stream.close()
 
-    return frames
+    return block
 
 
-def save(frames):
-    """
-    takes frames and saves them to a wav file
-    """
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+def check_trigger(block):
+    global trigger
+    amplitude = get_rms(block)
+    print(amplitude)
+    if amplitude > THRESHOLD:
+        trigger = True
+
+    return trigger
 
 
-def plot():
-    """
-    plots amplitude of audio input from wav file,
-    can also plot power over frequency
-    """
-    rate, data = wav.read(WAVE_OUTPUT_FILENAME)
-    t = np.arange(len(data[:, 0]))*1.0/rate
-    plt.plot(t, data[:, 0])
-    plt.show()
-
-    # power = 20*np.log10(np.abs(np.fft.rfft(data[:, 0])))
-    # f = np.linspace(0, rate/2.0, len(power))
-    # pl.plot(f, power)
-    # pl.xlabel("Frequency(Hz)")
-    # pl.ylabel("Power(dB)")
-    # pl.show()
-
-
-def maxAmplitude(wave_file):
-    """
-    finds max amplitude of a wave file
-    """
-    rate, data = wav.read(wave_file)
-    return np.amax(data[:, 0])
+def record(text=True):
+    global trigger
+    for i in range(70):
+        if text:
+            print("Recording")
+        if check_trigger(open_stream()):
+            print("TRIGGER")
+            trigger = False
+        if text:
+            print("Done Recording")
 
 
 def calibration():
@@ -95,26 +85,14 @@ def calibration():
 
     for i in range(3):
         print("Please SHOUT")
-        save(record(3))
-        shout_input.append(maxAmplitude(WAVE_OUTPUT_FILENAME))
+        time.sleep(2)
+        print("Recording")
+        for i in range(50):
+            shout_input.append(get_rms(open_stream()))
         print("Saving...")
     THRESHOLD = np.mean(shout_input)
     print("Done calibrating. Threshold set to: {}". format(THRESHOLD))
 
-
-def check_trigger():
-    """
-    Check to see if audio input goes above threshold,
-    returns bool
-    """
-    global trigger
-    save(record(RECORD_SECONDS))
-    max_amp = maxAmplitude(WAVE_OUTPUT_FILENAME)
-
-    print("Max Amplitude: {}".format(max_amp))
-    if max_amp >= THRESHOLD:
-        trigger = True
-    return trigger
 
 class recordingThread(threading.Thread):
     def __init__(self):
@@ -130,10 +108,8 @@ class recordingThread(threading.Thread):
             if check_trigger():
                 if recordingQueue.empty():
                     recordingQueue.put(True)
+
+
 if __name__ == "__main__":
     calibration()
-    for i in range(20):  # records for 10 seconds
-        if check_trigger():
-            print('FALL')
-            trigger = False
     p.terminate()
